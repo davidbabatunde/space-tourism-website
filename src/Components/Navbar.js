f7d6d781b474c3d6f39b467d41c3c96ad4bde312
Reol <reol0624@gmail.com>
2022-09-30 15:38:47 +0100

    added top nav and background with screen size

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -6,6 +6,31 @@ import hamburger from "../assets/shared/icon-hamburger.svg";
 import React from "react";
 
 function Navbar({ setMenu }) {
+  const handleChange = (r) => {
+    setMenu(false);
+    if (r === 0) {
+      document.getElementById("homeLink").classList.add("navActive");
+      document.getElementById("destinationLink").classList.remove("navActive");
+      document.getElementById("crewLink").classList.remove("navActive");
+      document.getElementById("technologyLink").classList.remove("navActive");
+    } else if (r === 1) {
+      document.getElementById("homeLink").classList.remove("navActive");
+      document.getElementById("destinationLink").classList.add("navActive");
+      document.getElementById("crewLink").classList.remove("navActive");
+      document.getElementById("technologyLink").classList.remove("navActive");
+    } else if (r === 2) {
+      document.getElementById("homeLink").classList.remove("navActive");
+      document.getElementById("destinationLink").classList.remove("navActive");
+      document.getElementById("crewLink").classList.add("navActive");
+      document.getElementById("technologyLink").classList.remove("navActive");
+    } else if (r === 3) {
+      document.getElementById("homeLink").classList.remove("navActive");
+      document.getElementById("destinationLink").classList.remove("navActive");
+      document.getElementById("crewLink").classList.remove("navActive");
+      document.getElementById("technologyLink").classList.add("navActive");
+    }
+  };
+
   return (
     <header>
       <img id="logo" src={logo} height="40px" alt="Company Logo" />
@@ -16,30 +41,30 @@ function Navbar({ setMenu }) {
         alt="menu button"
       />
       <ul id="menu-list">
-        <li>
-          <Link className="item" to="/" onClick={() => setMenu(false)}>
+        <li id="homeLink" className="navActive">
+          <Link className="item" to="/" onClick={() => handleChange(0)}>
             <span className="white">00</span>HOME
           </Link>
         </li>
-        <li>
+        <li id="destinationLink">
           <Link
             className="item"
             to="/destinations"
-            onClick={() => setMenu(false)}
+            onClick={() => handleChange(1)}
           >
             <span className="white">01</span>DESTINATION
           </Link>
         </li>
-        <li>
-          <Link className="item" to="/crew" onClick={() => setMenu(false)}>
+        <li id="crewLink">
+          <Link className="item" to="/crew" onClick={() => handleChange(2)}>
             <span className="white">02</span>CREW
           </Link>
         </li>
-        <li>
+        <li id="technologyLink">
           <Link
             className="item"
             to="/technology"
-            onClick={() => setMenu(false)}
+            onClick={() => handleChange(3)}
           >
             <span className="white">03</span>TECHNOLOGY
           </Link>
